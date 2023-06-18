63d32f006215a9993e210783d743b02353f33553
Ayeza <ayezabashir442@gmail.com>
2023-06-18 22:35:36 +0500

    fetching the movie details

--- js/main.js
+++ js/main.js
@@ -22,7 +22,7 @@ function getMovies(searchText) {
                        <img src="${movie.Poster}">
                        <div class="hidden">
                          <h5>${movie.Title}</h5>  
-                         <a class="btn btn-primary" onClick="movieSelected('${movie.imbdID}') href="#">Movie Details</a>
+                         <a class="btn btn-primary" onClick="movieSelected('${movie.imdbID}')" href="#">Movie Details</a>
                        </div>
                     </div>
                 </div>
@@ -33,6 +33,56 @@ function getMovies(searchText) {
 
         })
         .catch((err) => {
+            console.log(err);
+        })
+}
+
+function movieSelected(id) {
+    sessionStorage.setItem('movieId', id);
+    window.location = 'movieInfo.html';
+    return false;
+}
+
+function getMovie() {
+    let movieId = sessionStorage.getItem('movieId');
+
+    axios.get('http://www.omdbapi.com/?apikey=adbf0b5&i=' + movieId)
+        .then((resp) => {
+            console.log(resp);
+            let movie = resp.data;
+
+            let output = `
+            <div class="row">
+               <div class="col-md-4">
+                 <img src="${movie.Poster}" class="thumbnail">
+               </div>
+               <div class="col-md-8">
+                  <h2>${movie.Title}</h2>
+                  <ul class="list-group">
+                     <li class="list-group-item"><strong>Genre: </strong> ${movie.Genre}</li>
+                     <li class="list-group-item"><strong>Released: </strong> ${movie.Released}</li>
+                     <li class="list-group-item"><strong>Rated: </strong> ${movie.Rated}</li>
+                     <li class="list-group-item"><strong>IMDB Rating: </strong> ${movie.imdbRating}</li>
+                     <li class="list-group-item"><strong>Director: </strong> ${movie.Director}</li>
+                     <li class="list-group-item"><strong>Writer: </strong> ${movie.Writer}</li>
+                     <li class="list-group-item"><strong>Actors: </strong> ${movie.Actors}</li>
+                     <li class="list-group-item"><strong>Awards: </strong> ${movie.Awards}</li>
+                  </ul>
+               </div>
+            </div>
+            <div class="row mt-5">
+               <div class="well">
+                  <h3>Plot</h3>
+                  ${movie.Plot}
+                  <hr>
+                  <a href="http://imdb.com/title/${movie.imdbID}" target="_blank" class="btn btn-primary">View IMDB</a>
+                  <a href="index.html" class="btn btn-default">Go Back to Search</a>
+               </div>
+            </div>
+            `
+            $("#movie").html(output);
+        })
+        .catch(err => {
             console.log(err);
         })
 }
